357b6c87f8b8dc399ab7edc55701f2a6f449fb3c
Yuri <yuripetusko@gmail.com>
2024-10-04 11:35:16 +0200

    Update consts.ts

--- scripts/kusama-kingdom/consts.ts
+++ scripts/kusama-kingdom/consts.ts
@@ -2,6 +2,6 @@
 export const KUSAMA_API_URL_BASE = 'https://gql.rmrk.link/api/rest/collection-nfts';
 
 export const KUSAMA_KINGDOM_METADATA_IPFS_BASE =
-  'ipfs://QmPWs4tN9V2XwUrAo7r2Qrc1wcSP7hk8VdnwG54ajcDYcY';
+  'ipfs://Qmah863477cExZzYpLg5QYSgX2JEYYXGwWdsewnQeEtmo7';
 
 //https://gql.rmrk.link/api/rest/collection-nfts/2644199cf3652aaa78-KQ01/0
